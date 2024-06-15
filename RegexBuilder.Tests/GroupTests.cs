8655e4f77b4a674eb3e45b0be3d55f6c42393a10
Petannn <osc0011@vsb.cz>
2024-06-15 14:13:14 +0200

    group tests

--- RegexBuilder.Tests/GroupTests.cs
+++ RegexBuilder.Tests/GroupTests.cs
@@ -11,30 +11,131 @@ namespace RegexBuilder.Tests
 {
     internal class GroupTests
     {
-
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Atomic()
         {
             IGroup group = new AtomicGroup();
             group.AddCondition(new Literal("a"));
-            group.AddCondition(new Literal("ab"));
-            var regex = new Regex(group.ToRegexPattern()+"c");
-            Assert.False(regex.IsMatch("abc"));
+            group.AddCondition(new Literal("b"));
+            group.AddCondition(new Literal("c"));
+            Assert.AreEqual("(?>a|b|c)", group.ToRegexPattern());
         }
 
         [Test]
-        public void Atomic2()
+        public void Balancing()
         {
-            IGroup group = new AtomicGroup();
+            IGroup group = new BalancingGroup("ab", "cd");
             group.AddCondition(new Literal("a"));
-            group.AddCondition(new Literal("ab"));
-            var regex = new Regex(group.ToRegexPattern() + "c");
-            Assert.True(regex.IsMatch("ac"));
+            group.AddCondition(new Literal("b"));
+            group.AddCondition(new Literal("c"));
+            Assert.AreEqual("(?<ab-cd>abc)", group.ToRegexPattern());
         }
+
+        [Test]
+        public void Capturing()
+        {
+            IGroup group = new CapturingGroup();
+            group.AddCondition(new Literal("a"));
+            group.AddCondition(new Literal("b"));
+            group.AddCondition(new Literal("c"));
+            Assert.AreEqual("abc", group.ToRegexPattern());
+        }
+
+        [Test]
+        public void NonCapturing()
+        {
+            IGroup group = new NoncapturingGroup();
+            group.AddCondition(new Literal("a"));
+            group.AddCondition(new Literal("b"));
+            group.AddCondition(new Literal("c"));
+            Assert.AreEqual("(?:a|b|c)", group.ToRegexPattern());
+        }
+
+        [Test]
+        public void Matched()
+        {
+            IGroup group = new MatchedGroup();
+            group.AddCondition(new Literal("a"));
+            group.AddCondition(new Literal("b"));
+            group.AddCondition(new Literal("c"));
+            Assert.AreEqual("(abc)", group.ToRegexPattern());
+        }
+
+        [Test]
+        public void Nammed()
+        {
+            IGroup group = new NamedGroup("name");
+            group.AddCondition(new Literal("a"));
+            group.AddCondition(new Literal("b"));
+            group.AddCondition(new Literal("c"));
+            Assert.AreEqual("(?<name>abc)", group.ToRegexPattern());
+        }
+
+
+        [Test]
+        public void NegativeLookahead()
+        {
+            IGroup group = new NegativeLookaheadGroup();
+            group.AddCondition(new Literal("a"));
+            group.AddCondition(new Literal("b"));
+            group.AddCondition(new Literal("c"));
+            Assert.AreEqual("(?!abc)", group.ToRegexPattern());
+        }
+
+        [Test]
+        public void PositiveLookahead()
+        {
+            IGroup group = new PositiveLookaheadGroup();
+            group.AddCondition(new Literal("a"));
+            group.AddCondition(new Literal("b"));
+            group.AddCondition(new Literal("c"));
+            Assert.AreEqual("(?=abc)", group.ToRegexPattern());
+        }
+
+        [Test]
+        public void NegativeLookbehind()
+        {
+            IGroup group = new NegativeLookbehindGroup();
+            group.AddCondition(new Literal("a"));
+            group.AddCondition(new Literal("b"));
+            group.AddCondition(new Literal("c"));
+            Assert.AreEqual("(?<!abc)", group.ToRegexPattern());
+        }
+
+        [Test]
+        public void PositiveLookbehind()
+        {
+            IGroup group = new PositiveLookbehindGroup();
+            group.AddCondition(new Literal("a"));
+            group.AddCondition(new Literal("b"));
+            group.AddCondition(new Literal("c"));
+            Assert.AreEqual("(?<=abc)", group.ToRegexPattern());
+        }
+
+        [Test]
+        public void Options()
+        {
+            IGroup group = new OptionsGroup();
+            group.AddCondition(new Literal("a"));
+            group.AddCondition(new Literal("b"));
+            group.AddCondition(new Literal("c"));
+            Assert.AreEqual("(?imnsx-imnsx:abc)", group.ToRegexPattern());
+        }
+
+        [Test]
+        public void Nested()
+        {
+            IGroup named = new NamedGroup("name");
+            IGroup nested = new NoncapturingGroup();
+            
+            nested.AddCondition(new Literal("a"));
+            nested.AddCondition(new Literal("b"));
+            nested.AddCondition(new Literal("c"));
+
+            named.AddCondition(nested);
+
+            Assert.AreEqual("(?<name>(?:a|b|c))", named.ToRegexPattern());
+        }
+
     }
 }
