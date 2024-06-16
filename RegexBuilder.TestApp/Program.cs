f0eff8960f71cfb1fb4112525e4085771e51d4ff
Petannn <osc0011@vsb.cz>
2024-06-16 21:30:20 +0200

    extensions

--- RegexBuilder.TestApp/Program.cs
+++ RegexBuilder.TestApp/Program.cs
@@ -9,20 +9,21 @@ namespace RegexBuilder.TestApp
     {
         static void Main(string[] args)
         {
-            IRegexBuilder v = new RegexBuilder();
-            
-            IGroup group = new NamedGroup("world");
+            IRegexBuilder builder = new RegexBuilder();
 
-            group.AddCondition(new AnyWorld(MetaChars.Any));
-            v.AddCondition(group);
-            v.AddCondition(MetaChars.WhiteSpace, new Quantifier(1, null));
-            v.AddCondition(new BackReference("world"));
+            builder.Any(new Literal("ahoj"), new Literal("hi"), new Literal("ola"), new Literal("hello"));
+            builder.SkipWhiteSpaces();
+            builder.Any(new Literal("number"), new Literal("name"));
+            builder.SkipWhiteSpaces();
+            builder.Any(
+                 b => b.If(new Literal("number"), new AnyWord(MetaChars.AnyDigit)),
+                                b => b.If(new Literal("name"),   new AnyWord(MetaChars.WordCharacter)));
 
-            var r = v.Build();
+            var r = builder.Build();
             Console.WriteLine(r);
 
 
-            var matches = r.Matches("hello hello world world 1234 1234 ");
+            var matches = r.Matches("hello number 1234 1234 ");
 
             foreach (Match match in matches)
             {
