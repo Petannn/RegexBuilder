d596c0f58fd5839b9fa65a6e2c5a1dc456aa6cd6
Petannn <osc0011@vsb.cz>
2024-06-16 21:29:48 +0200

    refactoring

--- RegexBuilder/ConstrainBuilder.cs
+++ RegexBuilder/ConstrainBuilder.cs
@@ -9,24 +9,6 @@ using RegexBuilder.Conditions;
 
 namespace RegexBuilder
 {
-    public class Pattern
-    {
-        private readonly string _condition;
-        private readonly IQuantifier? _quantifier;
-
-        public Pattern(string condition, IQuantifier? quantifier = null)
-        {
-            _condition = condition;
-            _quantifier = quantifier;
-        }
-
-        public string Build()
-        {
-            return _quantifier == null ? _condition : _quantifier.ToRegexPattern(_condition);
-        }
-    }
-
-
     public class ConstrainBuilder<TCurrent, TInterface> : IConstrainBuilder<TCurrent, TInterface> 
         where TCurrent : ConstrainBuilder<TCurrent, TInterface>, TInterface
     {
---
+++ RegexBuilder/Pattern.cs
@@ -0,0 +1,18 @@
+﻿namespace RegexBuilder;
+
+public class Pattern
+{
+    private readonly string _condition;
+    private readonly IQuantifier? _quantifier;
+
+    public Pattern(string condition, IQuantifier? quantifier = null)
+    {
+        _condition = condition;
+        _quantifier = quantifier;
+    }
+
+    public string Build()
+    {
+        return _quantifier == null ? _condition : _quantifier.ToRegexPattern(_condition);
+    }
+}
